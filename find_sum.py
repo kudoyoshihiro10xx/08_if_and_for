fdb9bb379cd54bda297822ef08d3d5b6f5519b8d
Kudo <kudo@example.com>
2019-03-09 16:41:40 +0900

    find sum

---
+++ find_sum.py
@@ -0,0 +1,22 @@
+numbers = [34, 432, 1, 99]
+
+# numbersの合計値を for を使って出力してください
+total = 0
+total = total + numbers[0]
+total = total + numbers[1]
+total = total + numbers[2]
+total = total + numbers[3]
+
+print(total)
+
+total = 0
+for number in numbers:
+    total = total + number
+
+print(total)
+
+total = 0
+for number in numbers:
+    total += number
+
+print(total)
