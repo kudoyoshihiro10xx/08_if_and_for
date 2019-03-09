76bace063e647c6dd41137e9a0b56e7cd4030041
Kudo <kudo@example.com>
2019-03-09 17:17:21 +0900

    find max

---
+++ find_max].py
@@ -0,0 +1,16 @@
+numbers = [34, 432, 1, 99]
+
+champion = 0
+
+for number in numbers:
+    if number > champion:
+        champion = number
+
+print(champion)
+#
+# champion > 0
+#
+# if numbers[0] > champion:
+#     champion = number[0]
+
+
